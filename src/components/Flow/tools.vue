30b052254525a4682cbb71ec1449379e819ee3e2
GavinHome <yangxiaoming512@yeah.net>
2021-04-09 12:19:55 +0800

    update tools

--- src/components/Flow/tools.vue
+++ src/components/Flow/tools.vue
@@ -30,7 +30,7 @@
       >
       <el-button type="plain" size="small" @click="catData">查看数据</el-button>
       <el-button type="plain" size="small" @click="loadDemo"
-        >加载实例</el-button
+        >加载示例</el-button
       >
     </el-button-group>
   </div>
