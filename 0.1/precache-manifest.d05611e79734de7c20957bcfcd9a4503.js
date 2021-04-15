a34660ca7ee6fd32694c4c355c26025f2d068293
GavinHome <yangxiaoming512@yeah.net>
2021-04-15 14:21:39 +0800

    deploy gh-pages 0.1

---
+++ 0.1/precache-manifest.d05611e79734de7c20957bcfcd9a4503.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b92e308f0f02adaf4391",
+    "url": "/avigatorflow/css/app.d305b1ae.css"
+  },
+  {
+    "revision": "83be7046c4f53fba01af",
+    "url": "/avigatorflow/css/chunk-vendors.ae5c167e.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/avigatorflow/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/avigatorflow/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "cec096385a4b070cd4e661ec362092be",
+    "url": "/avigatorflow/img/end.cec09638.png"
+  },
+  {
+    "revision": "4f339cbe8b64cfcf50e537f5ba4e31a3",
+    "url": "/avigatorflow/img/user.4f339cbe.png"
+  },
+  {
+    "revision": "fab0c954cd8f2bd66263a464cc5e2165",
+    "url": "/avigatorflow/index.html"
+  },
+  {
+    "revision": "d0da8ba0b573e505ae69",
+    "url": "/avigatorflow/js/about.9f9faed7.js"
+  },
+  {
+    "revision": "b92e308f0f02adaf4391",
+    "url": "/avigatorflow/js/app.13b83bb2.js"
+  },
+  {
+    "revision": "83be7046c4f53fba01af",
+    "url": "/avigatorflow/js/chunk-vendors.96d36d76.js"
+  },
+  {
+    "revision": "af07b9e0958d681c55413a13941940fa",
+    "url": "/avigatorflow/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/avigatorflow/robots.txt"
+  }
+]);
