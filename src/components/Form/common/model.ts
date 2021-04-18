67ebd6fc897cfdb4d18018f06e2dfa71a1bee731
GavinHome <yangxiaoming512@yeah.net>
2021-04-18 10:02:58 +0800

    add widget type enum and model

---
+++ src/components/Form/common/model.ts
@@ -0,0 +1,188 @@
+export enum WidgetTypeEnum {
+  Label = "Label",
+  SingleText = "SingleText",
+  MultipleText = "MultipleText",
+  Number = "Number",
+  Date = "Date",
+  DateRange = "DateRange",
+  Select = "Select",
+  Radio = "Radio",
+  CheckBox = "CheckBox",
+}
+
+export function toWidgetText(type: WidgetTypeEnum): string {
+  switch (type) {
+    case WidgetTypeEnum.Label:
+      return "描述文本";
+    case WidgetTypeEnum.SingleText:
+      return "单行文本";
+    case WidgetTypeEnum.MultipleText:
+      return "多行文本";
+    case WidgetTypeEnum.Number:
+      return "数字";
+    case WidgetTypeEnum.Date:
+      return "日期";
+    case WidgetTypeEnum.DateRange:
+      return "日期区间";
+    case WidgetTypeEnum.Select:
+      return "下拉选择";
+    case WidgetTypeEnum.Radio:
+      return "单项选择";
+    case WidgetTypeEnum.CheckBox:
+      return "多项选择";
+    default:
+      throw new Error("field type enum error");
+  }
+}
+
+export interface WidgetNodeModel {
+  type: WidgetTypeEnum;
+  icon: string;
+  text: string;
+  name: string;
+}
+
+export interface WidgetSchema {
+  id: string;
+  code: string;
+  type: WidgetTypeEnum;
+  title: string;
+  placeHolder: string;
+  prompt: string;
+  description: string;
+  setting: FieldSettingModel;
+  //eslint-disable-next-line
+  value?: any;
+}
+
+///选项类型
+export interface DataOption {
+  value: string;
+  text: string;
+}
+
+export interface FieldSettingModel {
+  isRequired: boolean; //是否必填
+  isReadonly: boolean; //是否只读
+  isMultipleSelect?: boolean; //是否为多选，仅Select有效
+  maxStringLength?: number; //长度（文本）
+  maxNumberValue?: number; //最大值（数字）
+  minNumberValue?: number; //最小值（数字）
+  numberDigits?: number; //精度（数字）
+  unit?: string; //单位（数字）
+  isPercentage?: boolean; //百分比（数字）
+  //eslint-disable-next-line
+  defaultValue?: any; //默认值
+  // dataSourceType?: WidgetDataSourceTypeEnum;
+  options?: DataOption[];
+}
+
+export const componentsData: WidgetNodeModel[] = [
+  {
+    type: WidgetTypeEnum.Label,
+    icon: "label_icon.png",
+    text: toWidgetText(WidgetTypeEnum.Label),
+    name: "label",
+  },
+  {
+    type: WidgetTypeEnum.SingleText,
+    icon: "single_text_icon.png",
+    text: toWidgetText(WidgetTypeEnum.Label),
+    name: "a-input",
+  },
+  {
+    icon: "multiple_text_icon.png",
+    type: WidgetTypeEnum.MultipleText,
+    text: toWidgetText(WidgetTypeEnum.Label),
+    name: "a-textarea",
+  },
+  {
+    type: WidgetTypeEnum.Number,
+    icon: "number_icon.png",
+    text: toWidgetText(WidgetTypeEnum.Label),
+    name: "a-input-number",
+  },
+  {
+    type: WidgetTypeEnum.Date,
+    icon: "date_icon.png",
+    text: toWidgetText(WidgetTypeEnum.Label),
+    name: "a-date-picker",
+  },
+  {
+    type: WidgetTypeEnum.DateRange,
+    icon: "date_range_icon.png",
+    text: toWidgetText(WidgetTypeEnum.Label),
+    name: "a-range-picker",
+  },
+  {
+    type: WidgetTypeEnum.Select,
+    icon: "select_icon.png",
+    text: toWidgetText(WidgetTypeEnum.Label),
+    name: "a-select",
+  },
+  {
+    type: WidgetTypeEnum.Radio,
+    icon: "radio_icon.png",
+    text: toWidgetText(WidgetTypeEnum.Label),
+    name: "a-radio-group",
+  },
+  {
+    type: WidgetTypeEnum.CheckBox,
+    icon: "checkbox_icon.png",
+    text: toWidgetText(WidgetTypeEnum.Label),
+    name: "a-checkbox-group",
+  },
+];
+
+export function getComponentName(type: WidgetTypeEnum): string {
+  if (!type) {
+    return "";
+  }
+
+  const comp = componentsData.filter((x) => x.type == type)[0];
+  return comp ? comp.name : "";
+}
+
+export function canSetDataSource(
+  type: WidgetTypeEnum | null | undefined
+): boolean {
+  if (!type) {
+    return false;
+  }
+
+  const result = [
+    WidgetTypeEnum.Select,
+    WidgetTypeEnum.Radio,
+    WidgetTypeEnum.CheckBox,
+  ].includes(type);
+  return result;
+}
+
+export function getFieldTypeIcon(type: WidgetTypeEnum): string {
+  if (!type) {
+    return "";
+  }
+
+  // if (type == WidgetTypeEnum.Custom) {
+  //     return "custom_icon.png";
+  // }
+
+  const comp = componentsData.filter((x) => x.type == type)[0];
+  return comp ? comp.icon : "";
+}
+
+// export function getEmptyFieldInfo() {
+//     return {
+//         id: "",
+//         name: "",
+//         code: "",
+//         description: "",
+//         placeHolder: "",
+//         prompt: "",
+//         setting: {
+//             isMultipleSelect: false,
+//             isPercentage: false,
+//             options: [],
+//         },
+//     };
+// }
